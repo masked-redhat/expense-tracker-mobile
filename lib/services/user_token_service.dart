bbcffec2f72bbd42ddd2e9928890e46e4108b491
Zed <skhull.coding@proton.me>
2025-04-11 01:51:48 +0530

    chore: remove print statements

--- lib/services/user_token_service.dart
+++ lib/services/user_token_service.dart
@@ -3,7 +3,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 Future<void> saveUserToken(String token) async {
   final prefs = await SharedPreferences.getInstance();
   await prefs.setString('userToken', token);
-  print("Saved user token $token");
 }
 
 Future<String?> getUserToken() async {
